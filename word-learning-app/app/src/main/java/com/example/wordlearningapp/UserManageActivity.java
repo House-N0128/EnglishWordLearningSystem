4789fa0b9f9f7d3c52551ae7034c9537d8d92adf
House-N0128 <1748992171@qq.com>
2026-05-21 16:04:16 +0800

    修复编译错误: UserManageActivity恢复Button导入

--- word-learning-app/app/src/main/java/com/example/wordlearningapp/UserManageActivity.java
+++ word-learning-app/app/src/main/java/com/example/wordlearningapp/UserManageActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
